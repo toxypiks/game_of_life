f9670d28e76f7ad2e36ec58d368ce8c08b2339a4
toxypiks <HannaHinkelbein89@gmx.de>
2026-01-05 18:00:09 +0100

    Make main project scrollable

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include "raylib.h"
+#include "raymath.h"
+
 #include "game_of_life.h"
 #include "pixelbuffer.h"
 
@@ -22,8 +25,52 @@ int main()
 
     size_t pix_buf_idx = 0;
     initialize_pixelbuf(pixelbuffer[0], 0.3f);
+
+    // zoom stuff:
+    Camera2D camera = { 0 };
+    camera.zoom = 1.0f;
+    int zoomMode = 1;
+
     while(!WindowShouldClose())
     {
+        if (IsKeyPressed(KEY_ONE)) zoomMode = 0;
+        else if (IsKeyPressed(KEY_TWO)) zoomMode = 1;
+
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        {
+            Vector2 delta = GetMouseDelta();
+            delta = Vector2Scale(delta, -1.0f/camera.zoom);
+            camera.target = Vector2Add(camera.target, delta);
+        }
+
+        if (zoomMode == 0)
+        {
+            // Zoom based on mouse wheel
+            float wheel = GetMouseWheelMove();
+            if (wheel != 0)
+            {
+                Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+                camera.offset = GetMousePosition();
+                camera.target = mouseWorldPos;
+                float scale = 0.2f*wheel;
+                camera.zoom = Clamp(expf(logf(camera.zoom)+scale), 0.125f, 64.0f);
+            }
+        } else { // righclick zoom
+            if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+            {
+                Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+                camera.offset = GetMousePosition();
+                camera.target = mouseWorldPos;
+            }
+
+            if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+            {
+                float deltaX = GetMouseDelta().x;
+                float scale = 0.005f*deltaX;
+                camera.zoom = Clamp(expf(logf(camera.zoom)+scale), 0.125f, 64.0f);
+            }
+        }
+
         set_cell_states(pixelbuffer[pix_buf_idx], pixelbuffer[(pix_buf_idx == 0)]);
 
         Image img = {
@@ -38,11 +85,15 @@ int main()
         Texture2D texture = LoadTextureFromImage(img);
 
         BeginDrawing();
+        BeginMode2D(camera);
+
         ClearBackground(DARKBLUE);
         DrawTexture(texture, 0, 0, WHITE);
         ClearBackground(BLACK);
 
+        EndMode2D();
         EndDrawing();
+
         UnloadTexture(texture);
         pix_buf_idx = (pix_buf_idx == 1) ? 0 : 1;
     }
